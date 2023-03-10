47f2461ec8e682d45528d1993f6741b261ae402c
ThanhNV <thanhnv@mpos.vn>
2023-03-10 13:32:25 +0700

    commit code

---
+++ src/test/java/com/huylt/firstproject/FirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huylt.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
